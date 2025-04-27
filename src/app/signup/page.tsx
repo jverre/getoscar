769d9dacceb22546bb9d6107e98ca852a3c7aa83
Jacques <jverre@gmail.com>
2025-04-27 17:28:59 +0100

    WIP

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -13,15 +13,9 @@ export default function SignupPage() {
     const [error, setError] = useState<string | null>(null)
     
     const getURL = () => {
-        let url =
-            process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-            process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-            'http://localhost:3000/'
-        // Make sure to include `https://` when not localhost.
-        url = url.startsWith('http') ? url : `https://${url}`
-        // Make sure to include a trailing `/`.
-        url = url.endsWith('/') ? url : `${url}/`
-        return url
+        const origin = window.location.origin
+        // Make sure to include a trailing `/`
+        return origin.endsWith('/') ? origin : `${origin}/`
     }
 
     const handleEmailSignup = async (formData: FormData) => {
